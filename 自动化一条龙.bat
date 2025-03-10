90e6285431b5328991e0f4174b23602b6426c412
xzy-nine <xzyht-nine@outlook>
2025-03-10 21:24:09 +0800

    add:bat中添加检查依赖和更新 add:忽略日志文件

--- 自动化一条龙.bat
+++ 自动化一条龙.bat
@@ -1,6 +1,18 @@
 @echo off
 chcp 65001 >nul
 :: Unicode编码，解决中文乱码问题
+
+:: 检查并安装依赖项
+pip install -r requirements.txt
+
+:: 配置 sparse-checkout 以排除 json 文件
+git sparse-checkout init --cone
+git sparse-checkout set ':(exclude)*.json'
+
+:: 拉取更新
+git pull
+
+:: 运行主脚本
 python "./AutoGAME.py"
 
 pause
